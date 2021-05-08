57870adaec7b35f34f4b9502045a6b0db562e553
BlazarQSO <BlazarQSO@gmail.com>
2021-05-08 19:54:26 +0300

    refactor: add a description of the types

--- src/services/command.js
+++ src/services/command.js
@@ -5,10 +5,10 @@ const program = new Command();
 
 program
     .version('0.0.1')
-    .option('-s, --shift <type>', 'shift')
-    .option('-i, --input <type>', 'input')
-    .option('-o, --output <type>', 'output')
-    .option('-a, --action <type>', 'action')
+    .option('-s, --shift <number>', 'shift')
+    .option('-i, --input <file>', 'input')
+    .option('-o, --output <file>', 'output')
+    .option('-a, --action <encode|decode>', 'action')
     .configureOutput({
         writeOut: (str) => process.stdout.write(`[OUT] ${str}`),
         writeErr: (str) => process.stdout.write(Messager.errorCommand(str)),
